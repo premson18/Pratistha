84e815eb10fd8cfa559601d7e01a1ef5f546133c
Premson <premsonmunganda19@gmail.com>
2025-01-18 21:17:28 +0530

    first commit

--- src/Components/Sponsors/Sponsors.jsx
+++ src/Components/Sponsors/Sponsors.jsx
@@ -4,9 +4,38 @@ import './Sponsors.css'
 const Sponsors = () => {
   return (
     <div>
-      <div className='sponsors'>
-        
-      </div>
+      
+      <div className="wrapper">
+            <div className="item-1 item1"></div>
+            <div className="item-1 item2"></div>
+            <div className="item-1 item3"></div>
+            <div className="item-1 item4"></div>
+            <div className="item-1 item5"></div>
+            <div className="item-1 item6"></div>
+            <div className="item-1 item7"></div>
+            <div className="item-1 item8"></div>
+        </div>
+        <div className="wrapper">
+            <div className="item-2 item1"></div>
+            <div className="item-2 item2"></div>
+            <div className="item-2 item3"></div>
+            <div className="item-2 item4"></div>
+            <div className="item-2 item5"></div>
+            <div className="item-2 item6"></div>
+            <div className="item-2 item7"></div>
+            <div className="item-2 item8"></div>
+        </div>
+        <div className="wrapper">
+            <div className="item-3 item1"></div>
+            <div className="item-3 item2"></div>
+            <div className="item-3 item3"></div>
+            <div className="item-3 item4"></div>
+            <div className="item-3 item5"></div>
+            <div className="item-3 item6"></div>
+            <div className="item-3 item7"></div>
+            <div className="item-3 item8"></div>
+        </div>
+
     </div>
   )
 }
