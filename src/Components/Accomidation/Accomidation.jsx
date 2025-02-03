2a66192d8dbaad1a0928c00b85d47a2b542caff5
Premson <premsonmunganda19@gmail.com>
2025-02-03 13:49:40 +0530

    first commit

--- src/Components/Accomidation/Accomidation.jsx
+++ src/Components/Accomidation/Accomidation.jsx
@@ -10,7 +10,7 @@ const Accomidation = () => {
             <h1 className='accomh1'>Accomidation</h1>
             <p>✨ Welcome to JNTU-GV! ✨<br></br>
 
-We’re thrilled to offer 2 DAYS FREE ACCOMMODATION for participants of Pratistha-2K25!<br></br> Enjoy a comfortable stay, delicious meals, and a vibrant campus atmosphere.<br></br>
+We’re thrilled to offer 2 DAYS ACCOMMODATION for participants of Pratistha-2K25!<br></br> Enjoy a comfortable stay, delicious meals, and a vibrant campus atmosphere.<br></br>
 
 📅 Dates: March 3 & 4<br></br>
 📍 Location: Jntugv college,Dwarapudi,Vizianagaram,Andhra Pradesh 535003<br></br>
