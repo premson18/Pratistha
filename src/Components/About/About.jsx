9fdb16f2d3e9730f603d1a5150d354ce6054d66e
Premson <premsonmunganda19@gmail.com>
2025-02-06 22:56:36 +0530

    first commit

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -9,12 +9,12 @@ const About = () => {
         <div className="aboutcollege aboutcol">
           <h1 className='prat'>About College</h1>
           <img></img>
-          <p>The Civil Engineering Department at Jawaharlal Nehru Technological University - Gurajada Vizianagaram , is a hub of academic excellence, innovation, and practical learning. Committed to nurturing future leaders in the field of Civil engineering, the department offers a comprehensive curriculum that combines theoretical knowledge with hands-on experience. Renowned for its state-of-the-art infrastructure and cutting-edge laboratories, the department provides students with a conducive environment for research and development. </p>
+          <p className='aboutmat'>The Civil Engineering Department at Jawaharlal Nehru Technological University - Gurajada Vizianagaram , is a hub of academic excellence, innovation, and practical learning. Committed to nurturing future leaders in the field of Civil engineering, the department offers a comprehensive curriculum that combines theoretical knowledge with hands-on experience. Renowned for its state-of-the-art infrastructure and cutting-edge laboratories, the department provides students with a conducive environment for research and development. </p>
         </div>
         <div className="abotpratistha aboutcol">
           <h1 className='prat'>About Pratistha</h1>
           <img></img>
-           <p>Pratistha is the Two day National-level Technical Fest Organised by the Department of Civil Engineering in Which competition is conducted in various 
+           <p className='aboutmat'>Pratistha is the Two day National-level Technical Fest Organised by the Department of Civil Engineering in Which competition is conducted in various 
 
 Technical and Non-Technical events encourages students to participate and showcase their talents .Students put in their best entry learning how to manage time effect priotrize work, follow a schedule</p>
            </div>
