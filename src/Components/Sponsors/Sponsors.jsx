6e00472354a058fadd5d1484fe52c28cf2a76973
Premson <premsonmunganda19@gmail.com>
2025-02-11 22:16:34 +0530

    first commit

--- src/Components/Sponsors/Sponsors.jsx
+++ src/Components/Sponsors/Sponsors.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Sponsors.css'
-import MFC from '../../assets/Sponsors/mfc.png'
+
 
 
 const Sponsors = () => {
@@ -10,15 +10,15 @@ const Sponsors = () => {
         <h1 className='sponsorsh1'>Sponsors</h1>
       <div className="wrapper wrap1">
             <div className="item-1 item1 item12">
-              <img src={MFC}></img>
+              
             </div>
-            <div className="item-1 item2"></div>
-            <div className="item-1 item3"></div>
-            <div className="item-1 item4"></div>
-            <div className="item-1 item5"></div>
-            <div className="item-1 item6"></div>
-            <div className="item-1 item7"></div>
-            <div className="item-1 item8"></div>
+            <div className="item-1 item2 item12"></div>
+            <div className="item-1 item3 item12"></div>
+            <div className="item-1 item4 item12"></div>
+            <div className="item-1 item5 item12"></div>
+            <div className="item-1 item6 item12"></div>
+            <div className="item-1 item7 item12"></div>
+            <div className="item-1 item8 item12"></div>
         </div>
         <div className="wrapper wrap2">
             <div className="item-2 item1"></div>
