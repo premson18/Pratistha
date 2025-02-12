fccaf01becc6ca83c5a1cb2f0e2c514a44e57fc8
Premson <premsonmunganda19@gmail.com>
2025-02-12 22:29:47 +0530

    first commit

--- src/Components/Sponsors/Sponsors.jsx
+++ src/Components/Sponsors/Sponsors.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Sponsors.css'
-import MFC from "../../assets/Sponsors/mfc.jpeg"
+import MFC from "../../assets/Sponsors/mfc.png"
 
 
 
@@ -10,16 +10,14 @@ const Sponsors = () => {
       <section id='sponsors'>
         <h1 className='sponsorsh1'>Sponsors</h1>
       <div className="wrapper wrap1">
-            <div className="item-1 item1 item12">
-              
-            </div>
-            <div className="item-1 item2 item12"></div>
-            <div className="item-1 item3 item12"></div>
-            <div className="item-1 item4 item12"></div>
-            <div className="item-1 item5 item12"></div>
-            <div className="item-1 item6 item12"></div>
-            <div className="item-1 item7 item12"></div>
-            <div className="item-1 item8 item12"></div>
+           <div className="item-1 item1 item12"></div>
+            <div className="item-1 item2 "></div>
+            <div className="item-1 item3 "></div>
+            <div className="item-1 item4 "></div>
+            <div className="item-1 item5 "></div>
+            <div className="item-1 item6 "></div>
+            <div className="item-1 item7 "></div>
+            <div className="item-1 item8 "></div>
         </div>
         <div className="wrapper wrap2">
             <div className="item-2 item1"></div>
@@ -41,7 +39,7 @@ const Sponsors = () => {
             <div className="item-3 item7"></div>
             <div className="item-3 item8"></div>
         </div>
-
+       <div className='sponsors12'> <marquee direction="left" behavior="scroll" loop="infinite"><img src={MFC} ></img></marquee></div> 
       
         </section>
     </div>
