f242d2198033d72087b1aa191226bc75b8df15b2
Premson <premsonmunganda19@gmail.com>
2025-02-20 00:28:07 +0530

    first commit

--- src/Components/Workshop/Workshop.jsx
+++ src/Components/Workshop/Workshop.jsx
@@ -9,19 +9,19 @@ const Workshop = () => {
     <div>
       <section id='workshop123' className='container1'>
         <div className="workshop-card container1">
-         <div className='eventsinfo'> <h1>Workshop</h1><br></br>
+         <div className='eventsinfo'> <h1 className='workshop-title'>Workshop</h1><br></br>
       <div className="workshop ">
         <div className="workshop1">
           <h1 className='maintitle1'>Startup Spark</h1>
           <img src={WORKSHOP1} className="workshopimg"></img>
           <p className='classtext'>
-          <p><h1>Entrepreneurship:</h1> Discover the journey of     turning ideas into successful businesses.</p>
-<p><h1>Startup: </h1>Learn how startups disrupt industries and drive economic growth.</p>
-<p><h1>Stock Market:</h1> Dive into the exciting world of investments and financial markets.</p>
-<p><h1>Financial Planning:</h1> Financial planning involves managing income, expenses, savings, and investments to achieve long-term financial stability and goals.</p>
+          <p><h1 className='head'>Entrepreneurship:</h1> Discover the journey of     turning ideas into successful businesses.</p>
+<p><h1 className='head'>Startup: </h1>Learn how startups disrupt industries and drive economic growth.</p>
+<p><h1 className='head'>Stock Market:</h1> Dive into the exciting world of investments and financial markets.</p>
+<p><h1 className='head'>Financial Planning:</h1> Financial planning involves managing income, expenses, savings, and investments to achieve long-term financial stability and goals.</p>
           </p>
           <div >
-            <a><button className='price'>Price :- 600/-</button></a>
+            <a href='https://forms.gle/DTAF5ycCJ9GNuxks5' target="_blank" rel="noopener noreferrer"><button className='price1'>Price :- 600/-</button></a><br></br>
             <a className='price' href='https://forms.gle/DTAF5ycCJ9GNuxks5' target="_blank" rel="noopener noreferrer">
 <button>Register Here</button></a>
             
@@ -34,13 +34,12 @@ const Workshop = () => {
           Unlock the power of 3D design with SketchUp, the industry-leading modeling software. Whether you're an architect,<br></br> designer, or just curious about 3D modeling, this hands-on workshop will teach you the fundamentals of creating stunning visuals <br></br>and detailed structures. Learn from experts, enhance your creativity, and bring your ideas to life!
           </p>
           <div>
-          <a><button className='price'>Price :- 400/-</button></a><br></br>
+          <a href='https://forms.gle/TpXsBgmYRnLVUbD69' target="_blank" rel="noopener noreferrer"><button className='price1'>Price :- 400/-</button></a><br></br>
           <a className='price' href='https://forms.gle/TpXsBgmYRnLVUbD69' target="_blank" rel="noopener noreferrer">
-<button>Register Here</button></a>
+<button className='register'>Register Here</button></a>
         
           </div>
         </div>
-
       </div>
       </div>
       </div>
